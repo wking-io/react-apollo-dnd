664a7035530a4d24afadc9d278e936afc547216f
Will King <contact@wking.io>
2017-09-16 22:46:51 -0500

    Removed unneeded import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import './App.css';
 import Intro from './components/Intro';
 import ParentContainer from './components/ParentContainer';
 
